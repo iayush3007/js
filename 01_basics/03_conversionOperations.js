4b60b3f2772877701e296a0f62e394d35eb90166
iayush3007 <12212030@nitkkr.ac.in>
2025-06-29 13:33:58 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+let score = true
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1, false = 0
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+// 1 = true, 0 = false
+
+let someNumber = 33
+let numberToString = String(someNumber)
+console.log(typeof numberToString)
