8215e80f1f662edbf47606da8bce282c5dc82a31
iayush3007 <12212030@nitkkr.ac.in>
2025-06-29 15:20:30 +0000

    datatypes summary done

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,10 @@
+// console.log("2" > 1); true
+// console.log("02" > 1); true
+
+// console.log(null > 0) false
+// console.log(null < 0)  fasle
+// console.log(null >= 0)  comparison convert null to number, treating as 0 that's why it is giving true
+ 
+// === checks strictly(datatype also)
+console.log("2" === 2) //false
+
